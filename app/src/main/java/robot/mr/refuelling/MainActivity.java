7370980ad73c40714aa09dbf9f916f74f1e73ff1
ElliotUser <andersen.e@bk.ru>
2018-11-05 22:17:01 +0300

    Глобальные изменения

--- app/src/main/java/robot/mr/refuelling/MainActivity.java
+++ app/src/main/java/robot/mr/refuelling/MainActivity.java
@@ -1,48 +1,147 @@
 package robot.mr.refuelling;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import robot.mr.refuelling.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    EditText rightAmountInKg;
+    EditText theRemainingFuelInTheRightWing;
+    EditText fuelRemainingInTheCentralTank;
+    EditText theRemainingFuelInTheLeftWing;
+    EditText densityID;
+
+    Button buttonResult;
+    Button buttonClear;
+
+    TextView theRemainingFuelInKilograms;
+    TextView fuelRemainingInLiters;
+    TextView theAmountOfFuelToRefuelInLiters;
+    TextView theAmountOfFuelToRefuelInKg;
+
+    TextView leftWingTank;
+    TextView rightWingTank;
+    TextView centralWingTank;
+
+    String oper = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //находим элементы
+        rightAmountInKg = (EditText) findViewById(R.id.rightAmountInKg);
+        theRemainingFuelInTheLeftWing = (EditText) findViewById(R.id.theRemainingFuelInTheLeftWing);
+        theRemainingFuelInTheRightWing = (EditText) findViewById(R.id.theRemainingFuelInTheRightWing);
+        fuelRemainingInTheCentralTank = (EditText) findViewById(R.id.fuelRemainingInTheCentralTank);
+        densityID = (EditText) findViewById(R.id.densityID);
+
+        buttonResult = (Button)findViewById(R.id.buttonResult);
+        buttonClear = (Button)findViewById(R.id.buttonClear);
+
+        theRemainingFuelInKilograms = (TextView) findViewById(R.id.theRemainingFuelInKilograms);
+        fuelRemainingInLiters = (TextView) findViewById(R.id.fuelRemainingInLiters);
+        theAmountOfFuelToRefuelInLiters = (TextView) findViewById(R.id.theAmountOfFuelToRefuelInLiters);
+        theAmountOfFuelToRefuelInKg = (TextView) findViewById(R.id.theAmountOfFuelToRefuelInKg);
+        leftWingTank = (TextView) findViewById(R.id.leftWing);;
+        rightWingTank = (TextView) findViewById(R.id.rightWing);;
+        centralWingTank = (TextView) findViewById(R.id.centralTank);;
+
+        //прописываем обработчик
+        buttonResult.setOnClickListener(this);
+        buttonClear.setOnClickListener(this);
+
     }
 
-    public void onButtonClick(View view){
-        EditText rightAmountInKg = (EditText) findViewById(R.id.rightAmountInKg);
-        EditText theRemainingFuelInTheRightWing = (EditText) findViewById(R.id.theRemainingFuelInTheRightWing);
-        EditText fuelRemainingInTheCentralTank = (EditText) findViewById(R.id.fuelRemainingInTheCentralTank);
-        EditText theRemainingFuelInTheLeftWing = (EditText) findViewById(R.id.theRemainingFuelInTheLeftWing);
-        EditText densityID = (EditText) findViewById(R.id.densityID);
+    @Override
+    public void onClick(View v) {
+        //TODO Auto-generated method stub
+        float rightAmInKg = 0  ;
+        float leftWing = 0;
+        float centralTank = 0;
+        float rightWing = 0;
+        float denst = 0;
 
-        TextView theRemainingFuelInKilograms = (TextView) findViewById(R.id.theRemainingFuelInKilograms);
-        TextView fuelRemainingInLiters = (TextView) findViewById(R.id.fuelRemainingInLiters);
-        TextView theAmountOfFuelToRefuelInLiters = (TextView) findViewById(R.id.theAmountOfFuelToRefuelInLiters);
+        float resultRemainingFuelInKilograms = 0;
+        float resultRemainingFuelInLiters = 0;
+        float resultInKg = 0;
+        float resultInLiters = 0;
+
+        float resultLeftTank = 0;
+        float resultCentralTank = 0;
+        float resultRightTank = 0;
+
+        //Еще поделить на все баки!!!
+
+        //Проверяем поля на пустоту
+        if(TextUtils.isEmpty(rightAmountInKg.getText().toString()) ||
+                TextUtils.isEmpty(theRemainingFuelInTheLeftWing.getText().toString()) ||
+                TextUtils.isEmpty(fuelRemainingInTheCentralTank.getText().toString()) ||
+                TextUtils.isEmpty(theRemainingFuelInTheRightWing.getText().toString()) ||
+                TextUtils.isEmpty(densityID.getText().toString())){
+            return;
+        }
+
+        //читаем EditText и заполняем переменные числами
+        rightAmInKg = Float.parseFloat(rightAmountInKg.getText().toString());
+        leftWing = Float.parseFloat(theRemainingFuelInTheLeftWing.getText().toString());;
+        centralTank = Float.parseFloat(fuelRemainingInTheCentralTank.getText().toString());;
+        rightWing = Float.parseFloat(theRemainingFuelInTheRightWing.getText().toString());;
+        denst = Float.parseFloat(densityID.getText().toString());;
 
 
-        int rightAmInKg = Integer.parseInt(rightAmountInKg.getText().toString());
-        int leftWing = Integer.parseInt(theRemainingFuelInTheLeftWing.getText().toString());
-        int centrTank = Integer.parseInt(fuelRemainingInTheCentralTank.getText().toString());
-        int rightWing = Integer.parseInt(theRemainingFuelInTheRightWing.getText().toString());
-        double denst = Double.parseDouble(densityID.getText().toString());
+        switch(v.getId()){
+            case R.id.buttonResult:
+                resultRemainingFuelInKilograms = leftWing+centralTank+rightWing;
+                resultRemainingFuelInLiters = (leftWing+centralTank+rightWing)/denst;
+                resultInKg = (rightAmInKg-(leftWing+centralTank+rightWing));
+                resultInLiters = (rightAmInKg-(leftWing+centralTank+rightWing))/denst;
 
-        double requiredAmountOfFuel = (rightAmInKg-(leftWing+centrTank+rightWing))/denst;
-        theAmountOfFuelToRefuelInLiters.setText(Double.toString(requiredAmountOfFuel));
+                resultLeftTank = (float)(resultInKg*0.18);
+                resultRightTank = (float)(resultInKg*0.18);
+                resultCentralTank = (float) (resultInKg-(resultLeftTank+resultRightTank));
+                break;
 
-        int remainingKg = leftWing+centrTank+rightWing;
-        theRemainingFuelInKilograms.setText(Integer.toString(remainingKg));
+            case R.id.buttonClear:
+                rightAmountInKg.setText("");
+                theRemainingFuelInTheRightWing.setText("");
+                fuelRemainingInTheCentralTank.setText("");
+                theRemainingFuelInTheLeftWing.setText("");
+                densityID.setText("");
 
-        double remainingLitr = (leftWing+centrTank+rightWing)/denst;
-        fuelRemainingInLiters.setText(Double.toString(remainingLitr));
+                resultRemainingFuelInKilograms = 0;
+                resultRemainingFuelInLiters = 0;
+                resultInKg = 0;
+                resultInLiters = 0;
 
+                leftWingTank.setText("");
+                rightWingTank.setText("");
+                centralWingTank.setText("");
+                break;
+                default:
+                    break;
+        }
 
+        //строки вывода
+        theRemainingFuelInKilograms.setText(resultRemainingFuelInKilograms+"");
+        fuelRemainingInLiters.setText(resultRemainingFuelInLiters+"");
+        theAmountOfFuelToRefuelInLiters.setText(resultInLiters+"");
+        theAmountOfFuelToRefuelInKg.setText(resultInKg+"");
 
+        leftWingTank.setText(resultLeftTank+"");
+        rightWingTank.setText(resultRightTank+"");
+        centralWingTank.setText(resultCentralTank+"");
 
     }
 }
+
+
+
